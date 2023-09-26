0dacc53139d46bfc4aa71176ac289c31011929c9
truonglionstart7 <76563284+truonglionstart7@users.noreply.github.com>
2023-09-26 15:35:20 +0700

    Update Code

--- thong_ke.py
+++ thong_ke.py
@@ -21,10 +21,10 @@ kieusosanh = ["A", "B+", "B", "C+", "C", "D+", "D", "F"]
 dulieusosanh = np.array([np.sum(in_data[:,3]),np.sum(in_data[:,4]),np.sum(in_data[:,5])
                         ,np.sum(in_data[:,6]),np.sum(in_data[:,7]),np.sum(in_data[:,8])
                         ,np.sum(in_data[:,9]),np.sum(in_data[:,10])])
+myexplode = [0.2,0,0,0,0,0,0,0.2]
 fig1 = plt.figure()
-plt.pie(dulieusosanh, labels=kieusosanh, autopct='%1.1f%%')
+plt.pie(dulieusosanh, labels=kieusosanh,explode=myexplode, autopct='%1.1f%%')
 plt.axis('equal') 
-plt.show()
 
 maxa = diemA.max()
 i, = np.where(diemA == maxa)
