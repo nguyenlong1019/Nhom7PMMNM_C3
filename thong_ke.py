cb5f2f9c243281d7d87bb237859cc05ec4308e06
truonglionstart7 <76563284+truonglionstart7@users.noreply.github.com>
2023-09-30 17:47:16 +0700

    Update Code

--- thong_ke.py
+++ thong_ke.py
@@ -2,7 +2,9 @@ import pandas as pd
 from numpy import array
 import matplotlib.pyplot as plt
 import numpy as np
-df=pd.read_csv('E:/VD/Vidu_ma_nguon_mo-main/diemPython.csv',index_col=0,header = 0)
+
+df=pd.read_csv('diemPython.csv',index_col=0,header = 0)
+
 in_data = array(df.iloc[:,:])
 print(in_data)
 print('Tong so lop :')
@@ -23,19 +25,24 @@ dulieusosanh = np.array([np.sum(in_data[:,3]),np.sum(in_data[:,4]),np.sum(in_dat
                         ,np.sum(in_data[:,9]),np.sum(in_data[:,10])])
 myexplode = [0.2,0,0,0,0,0,0,0.2]
 fig1 = plt.figure()
-plt.pie(dulieusosanh, labels=kieusosanh,explode=myexplode, autopct='%1.1f%%')
+plt.pie(dulieusosanh, labels=kieusosanh,wedgeprops={'edgecolor':'white','linewidth':1.5},explode=myexplode, autopct='%1.1f%%')
+plt.legend(loc='upper left',bbox_to_anchor=(0.9, 1.1))
 plt.axis('equal') 
 
 maxa = diemA.max()
 i, = np.where(diemA == maxa)
 print("=========")
 print('lop co nhieu diem A la {0} co {1} sv dat diem A'.format(in_data[i,0],maxa))
+
 fig2 = plt.figure()
+kieusosanh1 = ["L1","L2","TX1","TX2","CK"]
+Solieu = np.array([np.sum(in_data[:,11]),np.sum(in_data[:,12]),np.sum(in_data[:,13]),np.sum(in_data[:,14]),np.sum(in_data[:,15])])
+plt.bar(kieusosanh1, Solieu,width=0.3)
+
+fig3 = plt.figure()
 plt.plot(range(len(diemA)),diemA,'r-',label="Diem A")
 plt.plot(range(len(diemBc)),diemBc,'g-',label="Diem B +")
 plt.xlabel('Lơp')
 plt.ylabel(' So sv dat diem ')
 plt.legend(loc='upper right')
 plt.show()
-
-      
