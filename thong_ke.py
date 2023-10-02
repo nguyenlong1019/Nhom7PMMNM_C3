8007ed590ec21eee5384add3d3b02bba0f506afc
tranbaquang22 <137389735+tranbaquang22@users.noreply.github.com>
2023-10-02 20:49:01 +0700

    Update thong_ke.py

--- thong_ke.py
+++ thong_ke.py
@@ -57,6 +57,90 @@ class App:
         
         self.style1 = ttk.Style()
         self.style1.configure("Frame2.TFrame",background="#f0f0f0")
+# ---------------------------------
+# Copyright by Tran Ba Quang
+# ---------------------------------
+# frame chứa định dạng báo cáo
+    def draw_ui(self):
+        # Tạo Frame chứa nội dung văn vản và hình ảnh với thanh cuộn
+        self.top_frame = Frame(self.root,width=950,height=600,borderwidth=1)
+        self.top_frame.pack(side=TOP)
+
+        # Canvas
+        self.canvas = Canvas(self.top_frame,background='#fff',width=900,height=600)
+        self.canvas.pack(side=LEFT)
+
+        # Scorllbar
+        self.y_scrollbar = ttk.Scrollbar(self.top_frame,orient=VERTICAL,command=self.canvas.yview)
+        self.y_scrollbar.pack(side=RIGHT,fill=Y)
+
+        self.canvas.configure(yscrollcommand=self.y_scrollbar.set)
+
+        # Content Frame
+        self.content_frame = ttk.Frame(self.canvas,width=900,height=1600,style="TFrame")
+        self.canvas.create_window((0,0),window=self.content_frame,anchor=NW)
+
+        
+        self.header_text = Label(self.content_frame,text='Báo cáo môn học Lập trình Python (FE6051)', anchor=CENTER, justify=CENTER, background='#fff')
+        self.header_text.configure(font=self.header_font)
+        self.header_text.place(x=450,y=20,anchor=CENTER)
+
+        self.body_text = Label(self.content_frame, text='I. Thông tin chung',background='#fff', font=self.text_font, anchor=CENTER, justify=CENTER)
+        self.body_text.place(x=50,y=100)
+
+        self.body_text1 = Label(self.content_frame, text='Tên Môn: Lập trình Python',background='#fff', font=self.text_font, anchor=CENTER, justify=CENTER)
+        self.body_text1.place(x=50,y=130)
+
+        self.body_text2 = Label(self.content_frame, text='Mã môn: FE6051        Số tín chỉ: 3 (2,1,0)',background='#fff', font=self.text_font, anchor=CENTER, justify=CENTER)
+        self.body_text2.place(x=50,y=160)
+
+        self.body_text3 = Label(self.content_frame, text='Số lớp học phần: 9',background='#fff', font=self.text_font, anchor=CENTER, justify=CENTER)
+        self.body_text3.place(x=50,y=190)
+
+        self.body_text4 = Label(self.content_frame, text='Tổng số sinh viên: 700        Pass: 83.2%',background='#fff', font=self.text_font, anchor=CENTER, justify=CENTER)
+        self.body_text4.place(x=50,y=220)
+        
+        self.body_text5 = Label(self.content_frame, text='II. Kết quả xử lý số liệu',background='#fff', font=self.text_font, anchor=CENTER, justify=CENTER)
+        self.body_text5.place(x=50,y=250)
+
+        self.image1 = Label(self.content_frame, image=self.score_ratio, background='#fff', anchor=CENTER, justify=CENTER)
+        self.image1.place(x=450,y=450,anchor=CENTER)
+
+        self.image2 = Label(self.content_frame,image=self.test_ratio,background='#fff',anchor=CENTER,justify=CENTER)
+        self.image2.place(x=450,y=800,anchor=CENTER)
+
+        self.body_text6 = Label(self.content_frame, text='III. Kết luận',background='#fff', font=self.text_font, anchor=CENTER, justify=CENTER)
+        self.body_text6.place(x=50,y=1000)
+
+        self.body_text7 = Label(self.content_frame, text='.'*79,background='#fff', font=self.text_font, anchor=CENTER, justify=CENTER)
+        self.body_text7.place(x=50,y=1030)
+
+        self.body_text8 = Label(self.content_frame, text='.'*79,background='#fff', font=self.text_font, anchor=CENTER, justify=CENTER)
+        self.body_text8.place(x=50,y=1060)
+
+        # end report
+        self.body_text9 = Label(self.content_frame, text='Ngày .... Tháng .... Năm .... ',background='#fff', font=self.text_font, anchor=CENTER, justify=CENTER)
+        self.body_text9.place(x=500,y=1100)
+
+        self.body_text9 = Label(self.content_frame, text='Ký tên',background='#fff', font=self.text_font, anchor=CENTER, justify=CENTER)
+        self.body_text9.place(x=575,y=1130)
+
+        self.content_frame.update_idletasks()
+        self.canvas.config(scrollregion=self.canvas.bbox("all"))
+        
+
+        # Tạo Frame chứa các Button điều khiển
+        self.control_frame = ttk.Frame(self.root,style='Frame2.TFrame')
+        self.control_frame.pack(fill=BOTH)
+
+        self.word_btn = ttk.Button(self.control_frame, text='Export Word',command=self.export_word)
+        self.word_btn.pack(side=LEFT,padx=10,pady=10,ipadx=10,ipady=5)
+
+        self.excel_btn = ttk.Button(self.control_frame,text='Export Excel',command=self.export_excel)
+        self.excel_btn.pack(side=LEFT,padx=10,pady=10,ipadx=10,ipady=5)
+    # ---------------------------------
+    # Copyright by Tran Ba Quang
+    # ---------------------------------
 
     
 
