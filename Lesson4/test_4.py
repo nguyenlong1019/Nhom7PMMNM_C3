7063b16cde1621c494bd7cea3db26cf89b1f8bea
nguyenlong1019 <nguyenvanlong17052002@gmail.com>
2023-10-10 16:09:00 +0700

    Lesson 5

---
+++ Lesson4/test_4.py
@@ -0,0 +1,19 @@
+from sympy import symbols
+from sympy.plotting import plot3d
+import random
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import sympy as sp
+import time
+import sys
+from tkinter import *
+from tkinter import ttk
+from matplotlib.image import imread
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+
+
+display_square_pyramid()
+
